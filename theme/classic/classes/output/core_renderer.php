e63c3f000db2b830080bd1c976564965af4e17d5
Michael Hawkins <michaelh@moodle.com>
2019-01-11 13:08:52 +0800

    MDL-64505 theme_classic: Classic theme cleanup and conversion for core

--- theme/classic/classes/output/core_renderer.php
+++ theme/classic/classes/output/core_renderer.php
@@ -1,5 +1,5 @@
 <?php
-// This file is part of the classic theme for Moodle
+// This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -14,12 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-namespace theme_classic\output;
-
-use \theme_boost\output\core_renderer as boost_core_renderer;
-
-defined('MOODLE_INTERNAL') || die;
-
 /**
  * Renderers to align Moodle's HTML with that expected by Bootstrap
  *
@@ -28,6 +22,20 @@ defined('MOODLE_INTERNAL') || die;
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-class core_renderer extends boost_core_renderer {
+namespace theme_classic\output;
+
+defined('MOODLE_INTERNAL') || die;
+
+/**
+ * Renderers to align Moodle's HTML with that expected by Bootstrap
+ *
+ * Note: This class is required to avoid inheriting Boost's core_renderer,
+ *       which removes the edit button required by Classic.
+ *
+ * @package    theme_classic
+ * @copyright  2018 Bas Brands
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class core_renderer extends \core_renderer {
 
 }
