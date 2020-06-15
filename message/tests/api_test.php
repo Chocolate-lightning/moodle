c4d7a1179d1682a6763b19189a4e7f156f74d7e4
Mathew May <mathewm@hotmail.co.nz>
2020-06-15 18:29:13 +0800

    fixup! MDL-63261 messaging: Remove deprercated services

--- message/tests/api_test.php
+++ message/tests/api_test.php
@@ -2351,83 +2351,6 @@ class core_message_api_testcase extends core_message_messagelib_testcase {
     }
 
     /**
-     * Tests retrieving contacts.
-     */
-    public function test_get_contacts() {
-        // Create some users.
-        $user1 = self::getDataGenerator()->create_user();
-
-        // Set as the user.
-        $this->setUser($user1);
-
-        $user2 = new stdClass();
-        $user2->firstname = 'User';
-        $user2->lastname = 'A';
-        $user2 = self::getDataGenerator()->create_user($user2);
-
-        $user3 = new stdClass();
-        $user3->firstname = 'User';
-        $user3->lastname = 'B';
-        $user3 = self::getDataGenerator()->create_user($user3);
-
-        $user4 = new stdClass();
-        $user4->firstname = 'User';
-        $user4->lastname = 'C';
-        $user4 = self::getDataGenerator()->create_user($user4);
-
-        $user5 = new stdClass();
-        $user5->firstname = 'User';
-        $user5->lastname = 'D';
-        $user5 = self::getDataGenerator()->create_user($user5);
-
-        // Add some users as contacts.
-        \core_message\api::add_contact($user1->id, $user2->id);
-        \core_message\api::add_contact($user1->id, $user3->id);
-        \core_message\api::add_contact($user1->id, $user4->id);
-
-        // Retrieve the contacts.
-        $contacts = \core_message\api::get_contacts($user1->id);
-
-        // Confirm the data is correct.
-        $this->assertEquals(3, count($contacts));
-        usort($contacts, ['static', 'sort_contacts']);
-
-        $contact1 = $contacts[0];
-        $contact2 = $contacts[1];
-        $contact3 = $contacts[2];
-
-        $this->assertEquals($user2->id, $contact1->userid);
-        $this->assertEmpty($contact1->useridfrom);
-        $this->assertFalse($contact1->ismessaging);
-        $this->assertNull($contact1->lastmessage);
-        $this->assertNull($contact1->messageid);
-        $this->assertNull($contact1->isonline);
-        $this->assertFalse($contact1->isread);
-        $this->assertFalse($contact1->isblocked);
-        $this->assertNull($contact1->unreadcount);
-
-        $this->assertEquals($user3->id, $contact2->userid);
-        $this->assertEmpty($contact2->useridfrom);
-        $this->assertFalse($contact2->ismessaging);
-        $this->assertNull($contact2->lastmessage);
-        $this->assertNull($contact2->messageid);
-        $this->assertNull($contact2->isonline);
-        $this->assertFalse($contact2->isread);
-        $this->assertFalse($contact2->isblocked);
-        $this->assertNull($contact2->unreadcount);
-
-        $this->assertEquals($user4->id, $contact3->userid);
-        $this->assertEmpty($contact3->useridfrom);
-        $this->assertFalse($contact3->ismessaging);
-        $this->assertNull($contact3->lastmessage);
-        $this->assertNull($contact3->messageid);
-        $this->assertNull($contact3->isonline);
-        $this->assertFalse($contact3->isread);
-        $this->assertFalse($contact3->isblocked);
-        $this->assertNull($contact3->unreadcount);
-    }
-
-    /**
      * Tests retrieving user contacts.
      */
     public function test_get_user_contacts() {
@@ -2485,56 +2408,6 @@ class core_message_api_testcase extends core_message_messagelib_testcase {
         $this->assertEquals($user4->id, $contact3->id);
         $this->assertEquals(fullname($user4), $contact3->fullname);
         $this->assertTrue($contact3->iscontact);
-    }
-
-    /**
-     * Tests retrieving messages.
-     */
-    public function test_get_messages() {
-        // Create some users.
-        $user1 = self::getDataGenerator()->create_user();
-        $user2 = self::getDataGenerator()->create_user();
-
-        // The person doing the search.
-        $this->setUser($user1);
-
-        // Send some messages back and forth.
-        $time = 1;
-        $this->send_fake_message($user1, $user2, 'Yo!', 0, $time + 1);
-        $this->send_fake_message($user2, $user1, 'Sup mang?', 0, $time + 2);
-        $this->send_fake_message($user1, $user2, 'Writing PHPUnit tests!', 0, $time + 3);
-        $this->send_fake_message($user2, $user1, 'Word.', 0, $time + 4);
-
-        // Retrieve the messages.
-        $messages = \core_message\api::get_messages($user1->id, $user2->id);
-
-        // Confirm the message data is correct.
-        $this->assertEquals(4, count($messages));
-
-        $message1 = $messages[0];
-        $message2 = $messages[1];
-        $message3 = $messages[2];
-        $message4 = $messages[3];
-
-        $this->assertEquals($user1->id, $message1->useridfrom);
-        $this->assertEquals($user2->id, $message1->useridto);
-        $this->assertTrue($message1->displayblocktime);
-        $this->assertContains('Yo!', $message1->text);
-
-        $this->assertEquals($user2->id, $message2->useridfrom);
-        $this->assertEquals($user1->id, $message2->useridto);
-        $this->assertFalse($message2->displayblocktime);
-        $this->assertContains('Sup mang?', $message2->text);
-
-        $this->assertEquals($user1->id, $message3->useridfrom);
-        $this->assertEquals($user2->id, $message3->useridto);
-        $this->assertFalse($message3->displayblocktime);
-        $this->assertContains('Writing PHPUnit tests!', $message3->text);
-
-        $this->assertEquals($user2->id, $message4->useridfrom);
-        $this->assertEquals($user1->id, $message4->useridto);
-        $this->assertFalse($message4->displayblocktime);
-        $this->assertContains('Word.', $message4->text);
     }
 
     /**
@@ -3010,33 +2883,6 @@ class core_message_api_testcase extends core_message_messagelib_testcase {
     }
 
     /**
-     * Tests retrieving most recent message.
-     */
-    public function test_get_most_recent_message() {
-        // Create some users.
-        $user1 = self::getDataGenerator()->create_user();
-        $user2 = self::getDataGenerator()->create_user();
-
-        // The person doing the search.
-        $this->setUser($user1);
-
-        // Send some messages back and forth.
-        $time = 1;
-        $this->send_fake_message($user1, $user2, 'Yo!', 0, $time + 1);
-        $this->send_fake_message($user2, $user1, 'Sup mang?', 0, $time + 2);
-        $this->send_fake_message($user1, $user2, 'Writing PHPUnit tests!', 0, $time + 3);
-        $this->send_fake_message($user2, $user1, 'Word.', 0, $time + 4);
-
-        // Retrieve the most recent messages.
-        $message = \core_message\api::get_most_recent_message($user1->id, $user2->id);
-
-        // Check the results are correct.
-        $this->assertEquals($user2->id, $message->useridfrom);
-        $this->assertEquals($user1->id, $message->useridto);
-        $this->assertContains('Word.', $message->text);
-    }
-
-    /**
      * Tests retrieving most recent conversation message.
      */
     public function test_get_most_recent_conversation_message() {
@@ -3067,34 +2913,6 @@ class core_message_api_testcase extends core_message_messagelib_testcase {
         // Check the results are correct.
         $this->assertEquals($user2->id, $message->useridfrom);
         $this->assertContains('Word.', $message->text);
-    }
-
-    /**
-     * Tests retrieving a user's profile.
-     */
-    public function test_get_profile() {
-        // Create some users.
-        $user1 = self::getDataGenerator()->create_user();
-
-        $user2 = new stdClass();
-        $user2->country = 'AU';
-        $user2->city = 'Perth';
-        $user2 = self::getDataGenerator()->create_user($user2);
-
-        // The person doing the search.
-        $this->setUser($user1);
-
-        // Get the profile.
-        $profile = \core_message\api::get_profile($user1->id, $user2->id);
-
-        $this->assertEquals($user2->id, $profile->userid);
-        $this->assertEmpty($profile->email);
-        $this->assertEmpty($profile->country);
-        $this->assertEmpty($profile->city);
-        $this->assertEquals(fullname($user2), $profile->fullname);
-        $this->assertNull($profile->isonline);
-        $this->assertFalse($profile->isblocked);
-        $this->assertFalse($profile->iscontact);
     }
 
     /**
