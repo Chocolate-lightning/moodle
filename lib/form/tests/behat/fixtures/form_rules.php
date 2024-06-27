34a51ef0062d4e56a1cc0fb5e81dfa1cf6d07295
Mathew May <mathewm@hotmail.co.nz>
2024-06-27 21:03:18 +0800

    Added basic tests ready for TDD

--- lib/form/tests/behat/fixtures/form_rules.php
+++ lib/form/tests/behat/fixtures/form_rules.php
@@ -46,7 +46,7 @@ class test_form_rules extends moodleform {
     public function definition() {
         $mform = $this->_form;
 
-        $mform->addElement('header', 'radioheader', 'Radio: eq/ne/neq/noteq');
+        $mform->addElement('header', 'radioheader', 'Radio: eq/neq');
         $radiogroup = [
             $mform->createElement('radio', 'radio_group_test', '', 'Enable', '1'),
             $mform->createElement('radio', 'radio_group_test', '', 'Disable', '2'),
@@ -55,21 +55,13 @@ class test_form_rules extends moodleform {
         $mform->addGroup($radiogroup, 'radio_group_test_group', 'Enable/Disable/Hide', ' ', false);
         $mform->setDefault('radio_group_test', 1);
 
-        $mform->addElement('button', 'some_eq_text', 'EQ');
+        $mform->addElement('button', 'some_eq_text', 'Radio EQ');
         $mform->disabledIf('some_eq_text', 'radio_group_test', 'eq', '2');
         $mform->hideIf('some_eq_text', 'radio_group_test', 'eq', '3');
 
-        $mform->addElement('button', 'some_neq_text', 'NEQ');
+        $mform->addElement('button', 'some_neq_text', 'Radio NEQ');
         $mform->disabledIf('some_neq_text', 'radio_group_test', 'neq', '2');
         $mform->hideIf('some_neq_text', 'radio_group_test', 'neq', '3');
-
-        $mform->addElement('button', 'some_ne_text', 'NE');
-        $mform->disabledIf('some_ne_text', 'radio_group_test', 'ne', '2');
-        $mform->hideIf('some_ne_text', 'radio_group_test', 'ne', '3');
-
-        $mform->addElement('button', 'some_noteq_text', 'NOTEQ');
-        $mform->disabledIf('some_noteq_text', 'radio_group_test', 'noteq', '2');
-        $mform->hideIf('some_noteq_text', 'radio_group_test', 'noteq', '3');
 
         $mform->addElement('header', 'checkboxheader', 'Checkboxes: checked/notchecked');
         $mform->setExpanded('checkboxheader');
@@ -93,53 +85,37 @@ class test_form_rules extends moodleform {
         $mform->addElement('button', 'some_unchecked_disabled_text', 'Not checked disabled');
         $mform->disabledIf('some_unchecked_disabled_text', 'some_disabled_uncheck_checkbox', 'notchecked');
 
-        $mform->addElement('header', 'textalphaheader', 'Text alpha: eq/ne/neq/noteq/in');
+        $mform->addElement('header', 'textalphaheader', 'Text alpha: eq/neq/in');
         $mform->setExpanded('textalphaheader');
         $mform->addElement('text', 'some_alpha_text', 'Text input');
         $mform->setType('some_alpha_text', PARAM_TEXT);
 
-        $mform->addElement('button', 'text_eq_text', 'EQ');
+        $mform->addElement('button', 'text_eq_text', 'Alpha EQ');
         $mform->disabledIf('text_eq_text', 'some_alpha_text', 'eq', 'Disable eq');
         $mform->hideIf('text_eq_text', 'some_alpha_text', 'eq', 'Hidden eq');
 
-        $mform->addElement('button', 'text_neq_text', 'NEQ');
+        $mform->addElement('button', 'text_neq_text', 'Alpha NEQ');
         $mform->disabledIf('text_neq_text', 'some_alpha_text', 'neq', 'Disable neq');
         $mform->hideIf('text_neq_text', 'some_alpha_text', 'neq', 'Hidden neq');
 
-        $mform->addElement('button', 'text_ne_text', 'NE');
-        $mform->disabledIf('text_ne_text', 'some_alpha_text', 'ne', 'Disable ne');
-        $mform->hideIf('text_ne_text', 'some_alpha_text', 'ne', 'Hidden ne');
+        $mform->addElement('button', 'text_in_text', 'Alpha IN');
+        $mform->disabledIf('text_in_text', 'some_alpha_text', 'in', ['Tool', 'Rage Against The Machine']);
+        $mform->hideIf('text_in_text', 'some_alpha_text', 'in', ['$uicideboy$', 'MF Doom', 'USAO / Camellia / No Mana']);
 
-        $mform->addElement('button', 'text_noteq_text', 'NOTEQ');
-        $mform->disabledIf('text_noteq_text', 'some_alpha_text', 'noteq', 'Disable noteq');
-        $mform->hideIf('text_noteq_text', 'some_alpha_text', 'noteq', 'Hidden noteq');
-
-        $mform->addElement('button', 'text_in_text', 'IN');
-        $mform->disabledIf('text_in_text', 'some_alpha_text', 'in', ['Tool', 'Rage Against The Machine', 'Future Islands']);
-        $mform->hideIf('text_in_text', 'some_alpha_text', 'in', ['SOAD', '$uicideboy$', 'MF Doom', 'USAO / Camellia / No Mana']);
-
-        $mform->addElement('header', 'textintheader', 'Text int: eq/ne/neq/noteq/in/??gt??');
+        $mform->addElement('header', 'textintheader', 'Text int: eq/neq/in');
         $mform->setExpanded('textintheader');
         $mform->addElement('text', 'some_int_text', 'Number input');
         $mform->setType('some_int_text', PARAM_INT);
 
-        $mform->addElement('button', 'int_eq_text', 'EQ');
+        $mform->addElement('button', 'int_eq_text', 'Int EQ');
         $mform->disabledIf('int_eq_text', 'some_int_text', 'eq', 1);
         $mform->hideIf('int_eq_text', 'some_int_text', 'eq', 2);
 
-        $mform->addElement('button', 'int_neq_text', 'NEQ');
+        $mform->addElement('button', 'int_neq_text', 'Int NEQ');
         $mform->disabledIf('int_neq_text', 'some_int_text', 'neq', 3);
         $mform->hideIf('int_neq_text', 'some_int_text', 'neq', 4);
 
-        $mform->addElement('button', 'int_ne_text', 'NE');
-        $mform->disabledIf('int_ne_text', 'some_int_text', 'ne', 5);
-        $mform->hideIf('int_ne_text', 'some_int_text', 'ne', 6);
-
-        $mform->addElement('button', 'int_noteq_text', 'NOTEQ');
-        $mform->disabledIf('int_noteq_text', 'some_int_text', 'noteq', 7);
-        $mform->hideIf('int_noteq_text', 'some_int_text', 'noteq', 8);
-
-        $mform->addElement('button', 'int_in_text', 'IN');
+        $mform->addElement('button', 'int_in_text', 'Int IN');
         $mform->disabledIf('int_in_text', 'some_int_text', 'in', [9, 10]);
         $mform->hideIf('int_in_text', 'some_int_text', 'in', [11, 12]);
 
